c4c7a8795f6f4ccf7d93e1200c18ce4ade5e084f
Ray <ll9907891@gmail.com>
2024-07-30 21:53:24 +0800

    初始化

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <>
+      <Button variant="destructive">Click me</Button>
+    </>
+  );
+}
+
+export default App;
